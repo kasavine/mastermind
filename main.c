951db03a83bd911ff1c1f701da68be52438de6ac
Ivanna Sak <kasavine@Ivannas-MacBook-Pro.local>
2021-03-07 13:26:50 -0800

    segfaulting

--- main.c
+++ main.c
@@ -12,21 +12,24 @@ int main(int ac, char **av)
     t_option* options = get_option(ac, av);
     while (i < ac){
         if (options->c == 1){
-            code = av[i + 1];
-            // code[4] = '\0';
+            printf("My printf for argument of -c: |%s|\n", av[i+1]);
         }
         else
             code = "1234";
+            printf("My printf if no argument: |%s|\n", code);
 
         if (options->p == 1)
-            pieces = av[i + 1];
+            printf("My printf for argument of -p: |%s|\n", av[i+1]);
         else
             pieces = "01234567";
+            printf("My printf if no argument: |%s|\n", pieces);
 
-        if (options->t == 1)
-            attempts = av[i + 1];
+        if (options->t == 1){
+            printf("My printf for argument of -t: |%s|\n", av[i+1]);
+        }
         else
             attempts = "10";
+            printf("My printf if no argument: |%s|\n", attempts);
         i++;
     }
     execute_game(code, pieces, atoi(attempts));
